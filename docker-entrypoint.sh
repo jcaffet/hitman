fee089a432266720a408431fc828a8f5fc9ace4a
Jerome Caffet <15911786+jcaffet@users.noreply.github.com>
2019-04-04 14:13:29 +0200

    check profile variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,14 +13,20 @@ aws s3 cp s3://${CONF_BUCKET}/${AWSNUKE_CONFIG_TEMPLATE} .
 echo "Set account to nuke : ${ACCOUNT_TO_NUKE} in ${AWSNUKE_CONFIG_TEMPLATE}"
 sed "s/||ACCOUNT||/${ACCOUNT_TO_NUKE}/g" ${AWSNUKE_CONFIG_TEMPLATE} > ${AWSNUKE_CONFIG}
 
-echo "Start nuking ${ACCOUNT_TO_NUKE} with ${NUKE_ROLE_TO_ASSUME} role"
+echo "Assume role ${NUKE_ROLE_TO_ASSUME} role on ${ACCOUNT_TO_NUKE}"
 aws sts assume-role --role-arn arn:aws:iam::${ACCOUNT_TO_NUKE}:role/${NUKE_ROLE_TO_ASSUME} \
 	            --role-session-name assumeRoleForNuke >${TMP_ASSUME_ROLE_FILE}
 
 export AWS_SECRET_ACCESS_KEY=`cat ${TMP_ASSUME_ROLE_FILE} | jq -r .Credentials.SecretAccessKey`
-export AWS_ACCESS_KEY_ID=`cat ${TMP_ASSUME_ROLE_FILE} | jq -r .Credentials.AccessKeyId`
-export AWS_SESSION_TOKEN=`cat ${TMP_ASSUME_ROLE_FILE} | jq -r .Credentials.SessionToken`
+if [ -z "${AWS_SECRET_ACCESS_KEY}" ]; then echo "AWS_SECRET_ACCESS_KEY not set !"; exit 1; fi
 
+export AWS_ACCESS_KEY_ID=`cat ${TMP_ASSUME_ROLE_FILE} | jq -r .Credentials.AccessKeyId`
+if [ -z "${AWS_ACCESS_KEY_ID}" ]; then echo "AWS_ACCESS_KEY_ID not set !"; exit 1; fi
+
+export AWS_SESSION_TOKEN=`cat ${TMP_ASSUME_ROLE_FILE} | jq -r .Credentials.SessionToken`
+if [ -z "${AWS_SESSION_TOKEN}" ]; then echo "AWS_SESSION_TOKEN not set !"; exit 1; fi
+
+echo "Start nuking ${ACCOUNT_TO_NUKE}"
 ${AWSNUKE_BIN} --config ${AWSNUKE_CONFIG} \
 	       --session-token ${AWS_SESSION_TOKEN} \
                --access-key-id ${AWS_ACCESS_KEY_ID} \
